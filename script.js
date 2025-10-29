8f83ed5d4aaecaf3385da4e7a15a8c3789994aee
Muhammad Mudasir <mudasirwindows@gmail.com>
2025-10-29 13:35:50 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+// 1. Collect input name from user.
+let naam = prompt("What's your name?");
+
+// 2. Show alert that name is submitted.
+alert("Your name [ " + naam + " ] ✅ has been successfully submitted!");
+
+// 3. Display your name on browser screen.
+document.write("<div><span>Hi, Welcome </span><i>" + naam + "</i></div>");
+
+// 4. Print your name on console window.
+console.log("Welcome,", naam);
